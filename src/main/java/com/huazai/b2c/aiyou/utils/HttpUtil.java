5bac6c3e698f973577c15ecf0cd86ce114c51928
huazai <who.seek.me@java98k.vip>
2019-05-01 18:36:24 +0800

    新增Http工具类

---
+++ src/main/java/com/huazai/b2c/aiyou/utils/HttpUtil.java
@@ -0,0 +1,19 @@
+package com.huazai.b2c.aiyou.utils;
+
+/**
+ * 
+ * @author HuaZai
+ * @contact who.seek.me@java98k.vip
+ *          <ul>
+ * @description TODO
+ *              </ul>
+ * @className HttpUtil
+ * @package com.huazai.b2c.aiyou.utils
+ * @createdTime 2019年5月1日 下午6:35:51
+ *
+ * @version V1.0.0
+ */
+public class HttpUtil
+{
+
+}
