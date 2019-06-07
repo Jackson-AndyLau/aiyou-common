9f087ed30d2165d45a1bf4ab85294f670cc48d69
huazai <who.seek.me@java98k.vip>
2019-06-07 10:18:52 +0800

    修改数据载体，数据集合名称

--- src/main/java/com/huazai/b2c/aiyou/common/EasyUIDataGrid.java
+++ src/main/java/com/huazai/b2c/aiyou/common/EasyUIDataGrid.java
@@ -19,14 +19,31 @@ import java.util.List;
 public class EasyUIDataGrid implements Serializable
 {
 
-	/** 
-	 * @Files serialVersionUID : TODO 
-	 */  
+	/**
+	 * @Files serialVersionUID : TODO
+	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * 总条数
+	 */
 	private Long total;
-
-	private List<?> list;
+	/**
+	 * 页大小
+	 */
+	private Integer pageSize;
+	/**
+	 * 当前页页码
+	 */
+	private Integer pageNum;
+	/**
+	 * 总页数
+	 */
+	private Integer pages;
+	/**
+	 * 分页数据集合
+	 */
+	private List<?> rows;
 
 	public Long getTotal()
 	{
@@ -38,14 +55,44 @@ public class EasyUIDataGrid implements Serializable
 		this.total = total;
 	}
 
-	public List<?> getList()
+	public List<?> getRows()
 	{
-		return list;
+		return rows;
 	}
 
-	public void setList(List<?> list)
+	public void setRows(List<?> rows)
 	{
-		this.list = list;
+		this.rows = rows;
+	}
+
+	public Integer getPageSize()
+	{
+		return pageSize;
+	}
+
+	public void setPageSize(Integer pageSize)
+	{
+		this.pageSize = pageSize;
+	}
+
+	public Integer getPageNum()
+	{
+		return pageNum;
+	}
+
+	public void setPageNum(Integer pageNum)
+	{
+		this.pageNum = pageNum;
+	}
+
+	public Integer getPages()
+	{
+		return pages;
+	}
+
+	public void setPages(Integer pages)
+	{
+		this.pages = pages;
 	}
 
 }
