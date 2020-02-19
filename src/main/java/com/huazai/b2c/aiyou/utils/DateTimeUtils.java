bf51a7cf087fe7137eeb16c2223e4bf2f703a54f
panda <dl.herbert@foxmail.com>
2020-02-19 21:17:04 +0800

    新增日期之间工具类

---
+++ src/main/java/com/huazai/b2c/aiyou/utils/DateTimeUtils.java
@@ -0,0 +1,40 @@
+package com.huazai.b2c.aiyou.utils;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author HuaZai
+ * @contact who.seek.me@java98k.vip
+ *          <ul>
+ * @description 时间格式化工具类
+ *              </ul>
+ * @className DateTimeUtils
+ * @package com.huazai.b2c.aiyou.utils
+ * @createdTime 2017年06月17日
+ *
+ * @version V1.0.0
+ */
+public class DateTimeUtils
+{
+
+	/**
+	 * 
+	 * @author HuaZai
+	 * @contact who.seek.me@java98k.vip
+	 * @title getCurrentDateTime
+	 *        <ul>
+	 * @description 获取当前日期时间
+	 *              </ul>
+	 * @createdTime 2017年06月17日
+	 * @return
+	 * @return Date
+	 *
+	 * @version : V1.0.0
+	 */
+	public static final Date getCurrentDateTime()
+	{
+
+		return new Date();
+	}
+}
